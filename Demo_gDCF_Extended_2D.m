75c24d4d9df82910ce6e183cbdae02ade541d754
OA-Bright <166884573+OA-Bright@users.noreply.github.com>
2025-12-31 10:52:12 -0600

    Update Demo_gDCF_Extended_2D.m

--- Demo_gDCF_Extended_2D.m
+++ Demo_gDCF_Extended_2D.m
@@ -1,11 +1,42 @@
 close all; clear all; clc
 
+p = gcp('nocreate');
+if isempty(p)
+numworkers =8;
+parpool('local', numworkers);
+end
+
 %Load in sample trajectory
 %data size = [Nreadout, Nshots, Ntime_frame]
 %trajectory is normalized within [-0.5 0.5]
 
 %Some example caculations
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Example 1
+data=load('./Radial_2D_36_spokes_GA.mat'); % enter filepath
+k_space_traj = data.k_rad; 
+size(k_space_traj)
+%recon matrix size
+N = 384;
+
+% Delta_k approximated with Nyquist
+    gDCF_1 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,0, 'Slow'); %no nufft correction - slow calc
+    gDCF_2 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,0, 'Fast'); %no nufft correction - Fast calc
+%Plot Traj and DCF for one timeframe 
+figure(1)
+plot(k_space_traj(:,:,1))
+title(['k-space Traj'])
+
+figure(2);
+hold on
+plot(gDCF_1(:,1,1),'r-','LineWidth',2.5,'DisplayName', 'gDCF-Nqy-Slow')
+plot(gDCF_2(:,1,1),'b--','LineWidth',3.5,'DisplayName', 'gDCF-Nqy-Fast')
+hold off
+legend('show')
+title(['gDCF-extended'])
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%Example 2
 data=load('./Radial_2D_36_spokes_GA.mat'); % enter filepath
 k_space_traj = data.k_rad; 
 size(k_space_traj)
@@ -14,20 +45,20 @@ N = 384;
 
 
 % Delta_k approximated with Nyquist
-    gDCF_1 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,0); %no nufft correction
-    gDCF_2 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,1); %nufft correction
+    gDCF_1 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,0, 'Fast'); %no nufft correction
+    gDCF_2 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,1, 'Fast'); %nufft correction
 
  % Delta_k approximated with BL
  % Note BL type = 0 (full spokes) or 1 (center-out readout)
-    gDCF_3 = gDCF_extended_2D(k_space_traj,N,'BL',0,0); % no nufft correction; 
-    gDCF_4 = gDCF_extended_2D(k_space_traj,N,'BL',0,1); % nufft correction; 
+    gDCF_3 = gDCF_extended_2D(k_space_traj,N,'BL',0,0, 'Fast'); % no nufft correction; 
+    gDCF_4 = gDCF_extended_2D(k_space_traj,N,'BL',0,1, 'Fast'); % nufft correction; 
 
 %Plot Traj and DCF for one timeframe 
-figure(1)
+figure(3)
 plot(k_space_traj(:,:,1))
 title(['k-space Traj'])
 
-figure(2);
+figure(4);
 hold on
 plot(gDCF_1(:,1,1),'r-','LineWidth',2.5,'DisplayName', 'gDCF-Nqy')
 plot(gDCF_2(:,1,1),'b--','LineWidth',3.5,'DisplayName', 'corr-gDCF-Nqy')
@@ -37,8 +68,8 @@ hold off
 legend('show')
 title(['gDCF-extended'])
 
-
-%Example 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Example 3
 data=load('./Spiral_10Shots_18TF'); % enter filepath
 k_space_traj = data.k_rad; 
 size(k_space_traj)
@@ -46,21 +77,21 @@ size(k_space_traj)
 N = 384;
 
 % Delta_k approximated with Nyquist
-    gDCF_1 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,1); %nufft correction
+    gDCF_1 = gDCF_extended_2D(k_space_traj,N,'Nyq',0,1, 'Fast'); %nufft correction
 
  % Delta_k approximated with BL
  % Note BL type = 0 (full spokes) or 1 (center-out readout)
-    gDCF_2 = gDCF_extended_2D(k_space_traj,N,'BL',1,1); % nufft correction; 
+    gDCF_2 = gDCF_extended_2D(k_space_traj,N,'BL',1,1, 'Fast'); % nufft correction; 
 
- % Delta_k approximated with Nyquist
-    gDCF_3 = gDCF_extended_2D(k_space_traj,N,'GEN',0,1); %nufft correction 
+ % Delta_k approximated with Gen-FOV
+    gDCF_3 = gDCF_extended_2D(k_space_traj,N,'GEN',0,1, 'Fast'); %nufft correction 
 
 %Plot Traj and DCF for one timeframe 
-figure(3)
+figure(5)
 plot(k_space_traj(:,:,1))
 title(['k-space Traj'])
 
-figure(4);
+figure(6);
 hold on
 plot(gDCF_1(:,1,1),'r-','LineWidth',2.5,'DisplayName', 'corr-gDCF-Nqy')
 plot(gDCF_2(:,1,1),'b--','LineWidth',3.5,'DisplayName', 'corr-gDCF-BL')
